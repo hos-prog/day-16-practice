6b3671e6eacfa790032caeaa2475aa2e265fe581
hos-prog <hosamaadem8@gmail.com>
2025-05-23 10:20:16 +0300

    Create turtle.py
    day 16 may 23 2025 GC

---
+++ turtle.py
@@ -0,0 +1,13 @@
+#HosamaAdem
+from turtle import Turtle,Screen
+import another_module
+print(another_module.another_variable)
+timmy=Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("coral")
+timmy.forward(100)
+
+my_screen=Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
